2093cbfe6f331b98abda1656dfb30e6358bc8d07
黄良运 <1136513099@qq.com>
2018-07-18 15:02:14 +0800

    manage

---
+++ HTML/src/main/test/test.java
@@ -0,0 +1,12 @@
+/**
+ * @author :hly
+ * @github :github.com/SiriusHly
+ * @blog :blog.csdn.net/Sirius_hly
+ * @date :2018/7/18
+ */
+public class test {
+    public static void main(String[]args){
+        int a[] = new int [5];
+
+    }
+}
